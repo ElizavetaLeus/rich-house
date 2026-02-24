d6ab660576af0411d4d7514f007df0f89bb60f91
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2026-02-24 16:00:56 +0300

    поправила настройки для элиаса

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-<TheHeader />
+  <TheHeader />
 </template>
 
 <script setup lang="ts">
-import TheHeader from './components/TheHeader.vue';
+import TheHeader from '@/components/TheHeader.vue';
 </script>
-
-
 
