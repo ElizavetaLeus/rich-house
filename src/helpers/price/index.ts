d72788536d00e92debcd0f0468afe4952e0b9fab
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2026-03-05 14:49:39 +0300

    сделала функцию-форматтер чисел

---
+++ src/helpers/price/index.ts
@@ -0,0 +1,7 @@
+export const priceFormatter = (price: number) => {
+  return new Intl.NumberFormat('ru-RU', {
+    style: 'decimal',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
+};
